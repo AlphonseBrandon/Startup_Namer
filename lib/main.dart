0c75fa4943489ac5c4e2a9c4395bd8bf07f07c3a
AlphonseBrandon <alphonse.brand@gmail.com>
2020-11-23 15:00:15 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Add this line.
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+            //child: Text('Hello World'),   // Replace this text...
+            // With this text.
+            ),
+      ),
+    );
+  }
+}
